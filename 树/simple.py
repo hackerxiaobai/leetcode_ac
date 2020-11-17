a8ca134d35d13f2640cadd129a8242257b877e26
hackerxiaobai <641082634@qq.com>
2020-11-17 19:47:05 +0800

    第四天树简单题

--- 树/simple.py
+++ 树/simple.py
@@ -379,6 +379,28 @@ class SimpleTopic(TreeNode):
         所以结果应当返回修剪好的二叉搜索树的新的根节点。注意，
         根节点可能会根据给定的边界发生改变。
         '''
+        def dfs(root, low, high):
+            if root is None:
+                return root
+            if root.val<low:
+                return dfs(root.right, low, high)
+            if root.val>high:
+                return dfs(root.left, low, high)
+            root.left = dfs(root.left, low, high)
+            root.right = dfs(root.right, low, high)
+            return root
+        return dfs(root, low, high)
+
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        '''
+        desc: 二叉树的堂兄弟节点
+        在二叉树中，根节点位于深度 0 处，每个深度为 k 的节点的子节点位于深度 k+1 处。
+        如果二叉树的两个节点深度相同，但父节点不同，则它们是一对堂兄弟节点。
+        我们给出了具有唯一值的二叉树的根节点 root，以及树中两个不同节点的值 x 和 y。
+        只有与值 x 和 y 对应的节点是堂兄弟节点时，才返回 true。否则，返回 false。
+        '''
+
+
 
 
 
