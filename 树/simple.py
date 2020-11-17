f4b44ee169843ad507758ef4bf29f7f98f02e69b
hackerxiaobai <641082634@qq.com>
2020-11-17 10:59:55 +0800

    第四天树简单题

--- 树/simple.py
+++ 树/simple.py
@@ -369,6 +369,18 @@ class SimpleTopic(TreeNode):
         self.res = root.val
         return dfs(root)
 
+    def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+        '''
+        desc: 修剪二叉搜索树
+        给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。
+        通过修剪二叉搜索树，使得所有节点的值在[low, high]中。
+        修剪树不应该改变保留在树中的元素的相对结构（即，如果没有被移除，
+        原有的父代子代关系都应当保留）。 可以证明，存在唯一的答案。
+        所以结果应当返回修剪好的二叉搜索树的新的根节点。注意，
+        根节点可能会根据给定的边界发生改变。
+        '''
+
+
 
 
 
