332a0bfbb27e553183b2976c7c304cab07fa02b3
hackerxiaobai <641082634@qq.com>
2020-11-18 20:33:43 +0800

    第五天树简单题

--- 树/simple.py
+++ 树/simple.py
@@ -440,10 +440,37 @@ class SimpleTopic(TreeNode):
             left = dfs(root.left)
             right = dfs(root.right)
             self.res = max(self.res, left+right+1)
-            return left+right+1
+            return max(left, right)+1
         self.res = 0
         dfs(root)
         return self.res -1
+
+    def findSecondMinimumValue(self, root: TreeNode) -> int:
+        '''
+        desc: 二叉树中第二小的节点
+        给定一个非空特殊的二叉树，每个节点都是正数，并且每个节点的子节点数量只能为 2 或 0。
+        如果一个节点有两个子节点的话，那么该节点的值等于两个子节点中较小的一个。
+        更正式地说，root.val = min(root.left.val, root.right.val) 总成立。
+        给出这样的一个二叉树，你需要输出所有节点中的第二小的值。
+        如果第二小的值不存在的话，输出 -1 。
+        '''
+        def dfs(root, m):
+            if not root:
+                return -1
+            if root.val>m:
+                return root.val
+            left = dfs(root.left, m)
+            right = dfs(root.right, m)
+            if left==-1:
+                return right
+            if right==-1:
+                return left
+            return min(left, right)
+        if not root:
+            return -1
+        return dfs(root, root.val)
+        
+
 
 
 
