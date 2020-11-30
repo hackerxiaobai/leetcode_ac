90b691afa5c11d9fb912473f24d05ab2fcc181f8
hackerxiaobai <641082634@qq.com>
2020-11-30 22:30:51 +0800

    双指针

--- 双指针/easy.py
+++ 双指针/easy.py
@@ -88,6 +88,20 @@ class Solution(ListNode):
             p -= 1
         nums1[:p2 + 1] = nums2[:p2 + 1]
 
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        desc: 移动零
+        给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+        """
+        n = len(nums)
+        left = right = 0
+        while right < n:
+            if nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+            right += 1
+        return nums
+
 
 
 
