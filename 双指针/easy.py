f2fdb109b73cb5e7988e2336ee43e4886c19a812
hackerxiaobai <641082634@qq.com>
2020-12-01 22:23:31 +0800

    双指针

--- 双指针/easy.py
+++ 双指针/easy.py
@@ -102,6 +102,43 @@ class Solution(ListNode):
             right += 1
         return nums
 
+    def backspaceCompare(self, S: str, T: str) -> bool:
+        '''
+        desc: 比较含退格的字符串
+        给定 S 和 T 两个字符串，当它们分别被输入到空白的文本编辑器后，
+        判断二者是否相等，并返回结果。 # 代表退格字符。
+        注意：如果对空文本输入退格字符，文本继续为空。
+        '''
+        i, j = len(S) - 1, len(T) - 1
+        skipS = skipT = 0
+        while i >= 0 or j >= 0:
+            while i >= 0:
+                if S[i] == "#":
+                    skipS += 1
+                    i -= 1
+                elif skipS > 0:
+                    skipS -= 1
+                    i -= 1
+                else:
+                    break
+            while j >= 0:
+                if T[j] == "#":
+                    skipT += 1
+                    j -= 1
+                elif skipT > 0:
+                    skipT -= 1
+                    j -= 1
+                else:
+                    break
+            if i >= 0 and j >= 0:
+                if S[i] != T[j]:
+                    return False
+            elif i >= 0 or j >= 0:
+                return False
+            i -= 1
+            j -= 1
+        return True
+
 
 
 
