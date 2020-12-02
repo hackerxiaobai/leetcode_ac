df5f665397488a51f76545db2ff4ebe82cfbc033
hackerxiaobai <641082634@qq.com>
2020-12-02 23:01:37 +0800

    双指针

--- 双指针/middle.py
+++ 双指针/middle.py
@@ -24,3 +24,26 @@ class Solution(ListNode):
                 else:
                     return ans
         return ans
+
+    def partition(self, head: ListNode, x: int) -> ListNode:
+    	'''
+		desc: 分隔链表
+		给定一个链表和一个特定值 x，对链表进行分隔，
+		使得所有小于 x 的节点都在大于或等于 x 的节点之前。
+		你应当保留两个分区中每个节点的初始相对位置。
+    	'''
+    	node1 = ListNode(None)
+        node2 = ListNode(None)
+        tmp1 = node1
+        tmp2 = node2
+        while head:
+            if head.val<x:
+                node1.next = ListNode(head.val)
+                node1 = node1.next
+                head = head.next
+            else:
+                node2.next = ListNode(head.val)
+                node2 = node2.next
+                head = head.next
+        node1.next = tmp2.next
+        return tmp1.next
