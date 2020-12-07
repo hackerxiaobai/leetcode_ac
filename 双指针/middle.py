169edc9cf1d94576576c7259e8545e4c1ca5a2e2
hackerxiaobai <641082634@qq.com>
2020-12-07 13:11:41 +0800

    双指针

--- 双指针/middle.py
+++ 双指针/middle.py
@@ -164,6 +164,27 @@ class Solution(ListNode):
                 j += 1
         return ans
 
+    def rotateRight(self, head: ListNode, k: int) -> ListNode:
+        '''
+        desc: 旋转链表
+        '''
+        if not head:
+            return None
+        if not head.next:
+            return head
+        old_tail = head
+        n = 1
+        while old_tail.next:
+            old_tail = old_tail.next
+            n += 1
+        old_tail.next = head
+        new_tail = head
+        for i in range(n - k % n - 1):
+            new_tail = new_tail.next
+        new_head = new_tail.next
+        new_tail.next = None
+        return new_head
+
 
 
 
