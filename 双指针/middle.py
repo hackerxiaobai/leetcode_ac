610f63f6cd90f734af80d3cb5d91fc72c7f7b776
hackerxiaobai <641082634@qq.com>
2020-12-15 00:03:44 +0800

    双指针

--- 双指针/middle.py
+++ 双指针/middle.py
@@ -367,7 +367,27 @@ class Solution(ListNode):
                 ans = max(ans, left[i] + right[i] + 1)
         return ans
 
-
+    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+        '''
+        desc: 长度最小的子数组
+        给定一个含有 n 个正整数的数组和一个正整数 s ，
+        找出该数组中满足其和 ≥ s 的长度最小的 连续 子数组，
+        并返回其长度。如果不存在符合条件的子数组，返回 0。
+        '''
+        if not nums:
+            return 0
+        n = len(nums)
+        ans = n + 1
+        start, end = 0, 0
+        total = 0
+        while end < n:
+            total += nums[end]
+            while total >= s:
+                ans = min(ans, end - start + 1)
+                total -= nums[start]
+                start += 1
+            end += 1
+        return 0 if ans == n + 1 else ans
 
 
 
