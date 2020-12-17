c5cf56bc5e348290843f7d438aa8583ac97b0e79
hackerxiaobai <641082634@qq.com>
2020-12-17 22:33:39 +0800

    双指针

--- 双指针/middle.py
+++ 双指针/middle.py
@@ -431,6 +431,22 @@ class Solution(ListNode):
                             res = str_d
         return res
 
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        '''
+        desc: 救生艇
+        第 i 个人的体重为 people[i]，每艘船可以承载的最大重量为 limit。
+        每艘船最多可同时载两人，但条件是这些人的重量之和最多为 limit。
+        返回载到每一个人所需的最小船数。(保证每个人都能被船载)。
+        '''
+        people.sort()
+        i, j = 0, len(people) - 1
+        ans = 0
+        while i <= j:
+            ans += 1
+            if people[i] + people[j] <= limit:
+                i += 1
+            j -= 1
+        return ans
 
 
 
