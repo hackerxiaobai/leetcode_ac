9e6d0b336792eb0e7dbb8dbd798aad4bf300c12e
hackerxiaobai <641082634@qq.com>
2020-12-20 22:21:29 +0800

    双指针

--- 双指针/middle.py
+++ 双指针/middle.py
@@ -483,6 +483,24 @@ class Solution(ListNode):
                 i = j       # 每次都更新 i即可指向word1也可指向word2               
         return ans
 
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        '''
+        desc: 删除子数组的最大得分
+        给你一个正整数数组 nums ，请你从中删除一个含有 若干不同元素 的子数组。删除子数组的 得分 就是子数组各元素之 和 。
+        返回 只删除一个 子数组可获得的 最大得分 。
+        如果数组 b 是数组 a 的一个连续子序列，即如果它等于 a[l],a[l+1],...,a[r] ，那么它就是 a 的一个子数组。
+        '''
+        pre_num = [0]
+        for num in nums:
+            pre_num.append(pre_num[-1] + num)
+        start, pos_dict, ans = 0, {}, 0
+        for i, n in enumerate(nums):
+            if n in pos_dict and pos_dict[n] >= start:
+                ans = max(ans, pre_num[i] - pre_num[start])
+                start = pos_dict[n] + 1
+            pos_dict[n] = i
+        return max(ans, pre_num[-1] - pre_num[start])
+
 
 
 
