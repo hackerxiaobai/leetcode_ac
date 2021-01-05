f5cb33aae7434327724397b6b8778d8b39ea43c2
hackerxiaobai <641082634@qq.com>
2021-01-05 22:21:15 +0800

    双指针

--- 双指针/middle.py
+++ 双指针/middle.py
@@ -521,6 +521,23 @@ class Solution(ListNode):
                 start = end + 1
         return ans
 
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        '''
+        desc: 乘积小于K的子数组
+        给定一个正整数数组 nums。
+        找出该数组内乘积小于 k 的连续的子数组的个数。
+        '''
+        if k <= 1: return 0
+        prod = 1
+        ans = left = 0
+        for right, val in enumerate(nums):
+            prod *= val
+            while prod >= k:
+                prod /= nums[left]
+                left += 1
+            ans += right - left + 1
+        return ans
+
 
 
 
